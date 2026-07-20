a4a5cb7bc2e4e3c5af48ea7642b7cc4d8d96fc53
Dedi Rialdi As Aldi <143459592+xylzn@users.noreply.github.com>
2026-07-20 18:27:04 +0700

    update graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     url: "https://selasar-bahasa.vercel.app",
     images: [
       {
-        url: "/og-image.png", // Taruh file 1200×630px di public/og-image.png
+        url: "public/og-image.png", // Taruh file 1200×630px di public/og-image.png
         width: 1200,
         height: 630,
         alt: "Selasar Bahasa",
